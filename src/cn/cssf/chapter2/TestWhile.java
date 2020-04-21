818a310489b7c5c04c65f1a8c7323f160de2a928
newonders <492329028@qq.com>
2020-04-21 09:19:56 +0800

    latest

---
+++ src/cn/cssf/chapter2/TestWhile.java
@@ -0,0 +1,17 @@
+package cn.cssf.chapter2;
+
+public class TestWhile {
+	public static void main(String[] args) {
+		int i = 0;
+		while(i < 10) {
+			System.out.println(i);
+			i++;
+		}
+		
+		i = 0;
+		do {
+			i++;
+			System.out.println(i);
+		} while(i < 10);
+	}
+}
